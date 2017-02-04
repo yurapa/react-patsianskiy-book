17c507f1b4f4f48956aeb0cd33d3f027e0a8de45
YuraPa <yura.paray@gmail.com>
2017-02-04 23:44:03 +0200

    create news from array

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 ### Summary ###
 Tutorial from boook
-
+Article [URL: habrahabr](https://habrahabr.ru/post/279249/)
 * Date: 2017-02-04 - ...
+* Read: page 40
+* Work: page 24
 
 ### Used technologies ###
 
 * HTML5+CSS3
 * JS
-* React
-* Babel
+* [React](https://facebook.github.io/react/)
+* [Babel](https://babeljs.io)
+
+### Commands ###
+* npm start -> run the project
--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,65 @@
+var my_news = [
+  {
+    author: "Sasha Pechkin",
+    text: "В четверг четвертого числа..."
+  },
+  {
+    author: "Просто Вася",
+    text: "Считаю что долар должен стоить 35 тугриков."
+  },
+  {
+    author: "Гость",
+    text: "Бесплатно скачать все - Лучший софт у нас!"
+  }
+];
+
+var News = React.createClass({
+  render: function() {
+    var data = this.props.data;
+    var newsTemplate = data.map(function(item, index) {
+      return (
+        <div key={index}>
+          <p className="news__author">{item.author}:</p>
+          <p className="news__text">{item.text}</p>
+        </div>
+      )
+    })
+    return (
+      <div className="news">
+        {newsTemplate}
+      </div>
+    );
+  }
+});
+
+var Comments = React.createClass({
+  render: function() {
+    return (
+      <div className="comments">
+        no comments here
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Hi for everyone, I'm App's component
+        <News data={my_news} /> {/* comments syntax */}
+        <Comments />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
 // console.log(React);
 // console.log(ReactDOM);
 
@@ -10,18 +72,3 @@
 //   <h1>Hello, next world!</h1>,
 //   document.getElementById('root')
 // );
-
-var App = React.createClass({
-  render: function() {
-    return (
-      <div className="app">
-        Hi for everyone, I'm App's component
-      </div>
-    );
-  }
-});
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
